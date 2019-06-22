89c1a08e7294c455ab192335595e5cc338b8ec88
phonypianist <phonypianist@gmail.com>
2019-06-23 00:44:06 +0900

    dispatcherを追加

---
+++ function-example/src/main/java/com/phonypianist/example/function/DispatchFunction.java
@@ -0,0 +1,29 @@
+package com.phonypianist.example.function;
+
+import com.phonypianist.example.function.dto.EchoRequest;
+import com.phonypianist.example.function.dto.EchoResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.function.Function;
+
+@Component("dispatch")
+public class DispatchFunction implements Function<EchoRequest, EchoResponse> {
+
+    private RestTemplate restTemplate;
+
+    private Environment environment;
+
+    public DispatchFunction(RestTemplate restTemplate, Environment environment) {
+        this.restTemplate = restTemplate;
+        this.environment = environment;
+    }
+
+    @Override
+    public EchoResponse apply(EchoRequest requestParam) {
+        String uri = environment.getProperty("APP_ECHO_URI");
+        return restTemplate.postForObject(uri, requestParam, EchoResponse.class);
+    }
+
+}
---
+++ function-example/src/main/java/com/phonypianist/example/function/EchoFluxFunction.java
@@ -0,0 +1,27 @@
+package com.phonypianist.example.function;
+
+import com.phonypianist.example.function.dto.EchoRequest;
+import com.phonypianist.example.function.dto.EchoResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+import java.util.function.Function;
+
+@Component("echoFlux")
+public class EchoFluxFunction implements Function<Flux<EchoRequest>, Flux<EchoResponse>> {
+
+    @Override
+    public Flux<EchoResponse> apply(Flux<EchoRequest> requestParam) {
+        return requestParam
+                .map(param -> {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return new EchoResponse(param.getMessage());
+                });
+    }
+
+}
