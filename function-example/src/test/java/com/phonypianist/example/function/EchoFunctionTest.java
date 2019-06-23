717117c3ae2a8507a78cad6434467c395ef78fac
phonypianist <phonypianist@gmail.com>
2019-06-23 23:50:01 +0900

    Normalなテストも追加

--- function-example/src/test/java/com/phonypianist/example/function/EchoFunctionTest.java
+++ function-example/src/test/java/com/phonypianist/example/function/EchoFunctionTest.java
@@ -3,6 +3,8 @@ package com.phonypianist.example.function;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.phonypianist.example.TestConfig;
+import com.phonypianist.example.function.dto.EchoRequest;
+import com.phonypianist.example.function.dto.EchoResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.cloud.function.adapter.aws.SpringBootApiGatewayRequestHandler;
 
@@ -12,12 +14,21 @@ class EchoFunctionTest {
 
     @Test
     void echo() {
+        EchoFunction echoFunction = new EchoFunction();
+        EchoRequest echoRequest = new EchoRequest();
+        echoRequest.setMessage("foo");
+        EchoResponse echoResponse = echoFunction.apply(echoRequest);
+        assertThat(echoResponse.getMessage()).isEqualTo("foo");
+    }
+
+    @Test
+    void echoWithSpring() {
         System.setProperty("function.name", "echo");
-        SpringBootApiGatewayRequestHandler requestHandler = new SpringBootApiGatewayRequestHandler(TestConfig.class);
-        APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent().withBody("{\"message\":\"foo\"}");
-        APIGatewayProxyResponseEvent responseEvent = (APIGatewayProxyResponseEvent) requestHandler.handleRequest(requestEvent, null);
-        requestHandler.close();
-        assertThat(responseEvent.getBody()).isEqualTo("{\"message\":\"foo\"}");
+        try (SpringBootApiGatewayRequestHandler requestHandler = new SpringBootApiGatewayRequestHandler(TestConfig.class)) {
+            APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent().withBody("{\"message\":\"foo\"}");
+            APIGatewayProxyResponseEvent responseEvent = (APIGatewayProxyResponseEvent) requestHandler.handleRequest(requestEvent, null);
+            assertThat(responseEvent.getBody()).isEqualTo("{\"message\":\"foo\"}");
+        }
     }
 
 }
