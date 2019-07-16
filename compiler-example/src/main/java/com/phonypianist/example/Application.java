3836a0875b40f8b6bf21584b6ef2926224bcba34
phonypianist <phonypianist@gmail.com>
2019-07-17 01:52:10 +0900

    Java11に変更

--- compiler-example/src/main/java/com/phonypianist/example/Application.java
+++ compiler-example/src/main/java/com/phonypianist/example/Application.java
@@ -2,7 +2,6 @@ package com.phonypianist.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.task.configuration.EnableTask;
 
 @SpringBootApplication
 public class Application {
